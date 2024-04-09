721ea3d562e6764e3be5a67743f88bb7b9fe195e
Kartik malik <sanjaykartik2222@gmail.com>
2024-04-09 18:16:23 +0530

    tech skills

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -1,20 +1,32 @@
+import edu_image from "../assets/education.gif";
 
 const Education = () => {
   return (
-    <div className="mb-8">
-      <h2 className="text-2xl font-bold mb-4">Education</h2>
-      <div className="mb-4">
-        <h3 className="text-lg font-semibold">Manav Rachna International Institute of Research and Studies</h3>
-        <p>Aug 2020 – June 2024</p>
-        <p>B. Tech in Computer Science & Engineering (Ongoing) 7.86 CGPA</p>
+    <>
+      <div className="p-6 mt-4">
+        <h1 className="text-6xl orbitron mb-4 flex flex-1 justify-center items-center">Education</h1>
+        <div className="flex flex-1 max-sm:flex-col justify-evenly items-center">
+        <div>
+          <img src={edu_image} width={400} alt="" />
+        </div>
+        <div className="mb-8">
+          <div className="mb-4">
+            <h3 className="text-lg font-semibold">
+              Manav Rachna International Institute of Research and Studies
+            </h3>
+            <p>Aug 2020 – June 2024</p>
+            <p>B. Tech in Computer Science & Engineering (Ongoing) 7.86 CGPA</p>
+          </div>
+          <div>
+            <h3 className="text-lg font-semibold">Tyagi Public School</h3>
+            <p>March 2018 – March 2020</p>
+            <p>Class 12th - 85%</p>
+            <p>Class 10th - 92%</p>
+          </div>
+        </div>
+        </div>
       </div>
-      <div>
-        <h3 className="text-lg font-semibold">Tyagi Public School</h3>
-        <p>March 2018 – March 2020</p>
-        <p>Class 12th - 85%</p>
-        <p>Class 10th - 92%</p>
-      </div>
-    </div>
+    </>
   );
 };
 
--- src/components/TechnicalSkills.tsx
+++ src/components/TechnicalSkills.tsx
@@ -1,11 +1,54 @@
+import skill_image from "../assets/wired-gradient-680-it-developer.gif";
 
 const TechnicalSkills = () => {
   return (
-    <div className="mb-8">
-      <h2 className="text-2xl font-bold mb-4">Technical Skills</h2>
-      <div>
-        <p>Languages: Python, C++, HTML, CSS, JavaScript, SQL</p>
-        <p>Technologies/Frameworks/Libraries: Beautifulsoup, Numpy, Matplotlib, Pandas, Seaborn, Tensorflow, Scikit, NLTK</p>
+    <div className="p-6 mt-4 bg-black text-white">
+      <h1 className="text-6xl orbitron flex flex-1 justify-center items-center ">
+        Technical Skills
+      </h1>
+      <div className="flex flex-1 justify-evenly items-center max-sm:flex-col-reverse">
+        <div>
+          <h1 className="flex flex-1 justify-center pb-2 text-2xl poppins-semibold">
+            Languages
+          </h1>
+          <hr />
+          <br />
+          <div className="flex flex-1 justify-evenly cursor-pointer">
+            <p className="montserrat">Python</p>
+            <p className="montserrat">C++</p>
+            <p className="montserrat">HTML</p>
+            <p className="montserrat">CSS</p>
+            <p className="montserrat">JavaScript</p>
+            <p className="montserrat">SQL</p>
+          </div>
+          <br />
+          <hr />
+          <br />
+          <h1 className=" mt-6 flex justify-center text-2xl pb-2 poppins-semibold">
+            Technologies/Frameworks/Libraries
+          </h1>
+          <hr />
+          <br />
+          <div className="flex flex-col flex-1 justify-center cursor-pointer">
+            <div className="flex flex-row flex-1 justify-evenly cursor-pointer">
+              <p className="montserrat">Beautifulsoup</p>
+              <p className="montserrat">Numpy</p>
+              <p className="montserrat">Matplotlib</p>
+              <p className="montserrat">Pandas</p>
+            </div>
+            <div className="flex flex-row flex-1 justify-evenly cursor-pointer">
+              <p className="montserrat">Seaborn</p>
+              <p className="montserrat">Tensorflow</p>
+              <p className="montserrat">Scikit</p>
+              <p className="montserrat">NLTK</p>
+            </div>
+          </div>
+          <br />
+          <hr />
+        </div>
+        <div>
+          <img src={skill_image} width={400} alt="" />
+        </div>
       </div>
     </div>
   );
